a9f40f6731e3f6c4d72931600b165763562a0f8f
itsramhere <ram.iyer200722@gmail.com>
2025-12-03 22:56:45 +0530

    [any message]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,5 +3,11 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) throws Exception {
         System.out.print("$ ");
+        Scanner sc = new Scanner(System.in);
+        String line = sc.nextLine();
+
+        System.out.println(line +": command not found");
+
+        sc.close();
     }
 }
