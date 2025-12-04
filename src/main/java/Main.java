aebb5b8ccd37aa862ee266c9fe3f32bfe4bd1dd2
itsramhere <ram.iyer200722@gmail.com>
2025-12-04 09:27:30 +0530

    Testing echo function

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -9,6 +9,12 @@ public class Main {
             if (line.equals("exit")) {
                 break;
             }
+            String [] parts = line.trim().split(" ");
+            if(parts.length > 0 && parts[0].equals("echo")){
+                for(int i=1; i<parts.length; i++){
+                    System.out.print(parts[i] + " ");
+                }
+            }
             System.out.println(line +": command not found");
         }
         sc.close();
