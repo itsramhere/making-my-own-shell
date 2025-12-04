50af53ae0bcd4ca9536bc2c7bf6e4555d183b7dd
itsramhere <ram.iyer200722@gmail.com>
2025-12-04 13:45:08 +0530

    pwd test 2

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,7 +3,7 @@ import java.util.*;
 
 public class Main {
 
-    private static String[] validCommands = {"echo", "exit", "type"};
+    private static String[] validCommands = {"echo", "exit", "type", "pwd"};
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
