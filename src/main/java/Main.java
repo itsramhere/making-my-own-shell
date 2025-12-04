6799be6be4a98d1214d752bd3bd662f636775aca
itsramhere <ram.iyer200722@gmail.com>
2025-12-04 09:15:19 +0530

    [any message]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,9 +2,8 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-        System.out.print("$ ");
         Scanner sc = new Scanner(System.in);
-        while (1) {
+        while (true) {
             System.out.print("$ ");
             String line = sc.nextLine();
             System.out.println(line +": command not found");
